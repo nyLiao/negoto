3d596b05482c018bc22a1513843433854c7757ae
nyLiao <39255546+nyLiao@users.noreply.github.com>
2020-04-18 01:48:46 +0800

    Runnable demo of basic ui and generate function

--- js/renderer.js
+++ js/renderer.js
@@ -4,25 +4,39 @@ var userService = require('./gen_nodejs/userService.js');
 var thriftConnection = thrift.createConnection('127.0.0.1', 6161);
 var thriftClient = thrift.createClient(userService, thriftConnection);
 
-thriftConnection.on("error", function(e)
-{
+thriftConnection.on("error", function(e) {
     console.log(e);
 });
 
 // Set up variables
-let txtin = document.querySelector('#txtin')
-let result = document.querySelector('#result')
+let txtin = document.querySelector('#txtin1')
+let result = document.querySelector('#result1')
+let btnGen = document.getElementById('btnGen')
 
 // Call function
-txtin.addEventListener('input', () => {
-  var dic = {txtin: txtin.value}
-  dic = JSON.stringify(dic)
-  thriftClient.test1(dic, (error, res) => {
-    if(error) {
-      console.error(error)
-    } else {
-      result.textContent = res
+btnGen.addEventListener('click', () => {
+    var dic = {
+        txtin: txtin.value
     }
-  })
+    dic = JSON.stringify(dic)
+
+    // Show loading animation
+    document.getElementById('lamp').className = "icon icon-lamp loading"
+    btnGen.disabled = true
+
+    // Get result
+    result.value = ''
+    thriftClient.test1(dic, (error, res) => {
+        if (error) {
+            console.error(error)
+        } else {
+            result.value = res
+        }
+    })
 })
-txtin.dispatchEvent(new Event('input'))
+
+// Stop loading animation
+result.onclick = () => {
+    document.getElementById('lamp').className = "icon icon-lamp"
+    btnGen.disabled = false
+}
--- python/api.py
+++ python/api.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+# API to run call python generating methods and handle transfer.
+#
+# by nyLiao, 2020
+
 import sys
 # import glob
 sys.path.append('gen_py')
 # sys.path.append('../')
-# sys.path.insert(0, glob.glob('../../lib/py/build/lib*')[0])
+# sys.path.insert(0, glob.glob('../lib/py/build/lib*')[0])
 
 import json
+import time
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
@@ -12,20 +18,30 @@ from thrift.server import TServer
 
 from gen_py.api import userService
 
+sys.path.append('../GPT2-Chinese/')
+from generate_class import genModel
 
-class Test:
-    count = 0
+# class Test:
+#     count = 0
+#
+#     def test1(self, dic):
+#         dic = json.loads(dic)
+#         self.count += 1
+#         strr = 'Hello x{}, {}!'.format(self.count, dic["txtin"])
+#         time.sleep(2)
+#         return strr
 
+class Test(genModel):
     def test1(self, dic):
         dic = json.loads(dic)
-        self.count += 1
-        strr = 'Hello x{}, {}!'.format(self.count, dic["txtin"])
+        inputs = dic["txtin"]
+        strr = self.gen_ph(inputs, length=300, topk=8, topp=1)
         return strr
 
 
 if __name__ == "__main__":
     # Init class process
-    handler = Test()
+    handler = Test(model_path='../GPT2-Chinese//model/model_epoch7', tokenizer_path='../GPT2-Chinese//model/model_epoch7/vocab.txt')
     processor = userService.Processor(handler)
 
     # Transport layer
